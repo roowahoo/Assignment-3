25af11e76a00c6602765f34ea5417566d5b4549a
roowahoo <tohyinru95@gmail.com>
2021-04-06 03:56:07 +0000

    created createProductForm

---
+++ forms/index.js
@@ -0,0 +1,122 @@
+const forms = require('forms')
+const fields=forms.fields
+const validators = forms.validators
+const widget = widgets = require('forms').widgets;
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+const createProductForm = (categories, tags) => {
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'directions': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'ingredients': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+
+        'net_weight': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators': [validators.integer()]
+        }),
+        'price': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators': [validators.integer()]
+        }),
+        'stock': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators': [validators.integer()]
+        }),
+        'date_of_manufacture': fields.date({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            widget: widget.date()
+        }),
+
+        // 'category_id': fields.string({
+        //     label: 'Category',
+        //     required: true,
+        //     errorAfterField: true,
+        //     cssClasses: {
+        //         label: ['form-label']
+        //     },
+        //     widget: widgets.select(),
+        //     choices: categories
+        // }),
+        // 'tags': fields.string({
+        //     label: 'Tags',
+        //     required: true,
+        //     errorAfterField: true,
+        //     cssClasses: {
+        //         label: ['form-label']
+        //     },
+        //     widget: forms.widgets.multipleSelect(),
+        //     //choices is set to all tags from the 'tags' table
+        //     choices: tags
+        // }),
+        // 'image_url':fields.string({
+        //     required:true,
+        //     errorAfterField:true,
+        //     widget:widgets.hidden()
+
+        // })
+
+
+    })
+};
+
+module.exports = { createProductForm, bootstrapField };
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express =require('express')
 const router=express.Router()
 
 const {Product}=require('../models')
+const { bootstrapField, createProductForm } = require('../forms')
 
 router.get('/',async (req,res)=>{
     let products=await Product.collection().fetch()
@@ -9,5 +10,12 @@ router.get('/',async (req,res)=>{
         'products':products.toJSON()
     })
     console.log(products.toJSON())
+})
+
+router.get('/create', async (req, res) => {
+    const productForm = createProductForm();
+    res.render('products/create',{
+        'form': productForm.toHTML(bootstrapField)
+    })
 })
 module.exports=router
