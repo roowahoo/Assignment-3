57bae05ffccf21faa0f70adc05e68d9f2b0173c1
roowahoo <tohyinru95@gmail.com>
2021-04-09 02:29:27 +0000

    added user registration

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,31 @@ router.get('/register', (req, res) => {
     })
 })
 
+router.post('/register',(req,res)=>{
+    const registrationForm=createRegistrationForm()
+    registrationForm.handle(req,{
+        'success':async(form)=>{
+            const user=new User({
+                'username':form.data.username,
+                'email':form.data.email,
+                'password':form.data.password
+            })
+            await user.save()
+            req.flash('success_messages','Thank you for signing up')
+            res.redirect('/users/login')
+        },
+        'error':async(form)=>{
+            res.render('users/register',{
+                'form':form.toHTML(bootstrapField)
+            })
+        }
+    })
+})
+
+router.get('/login', (req, res) => {
+    
+    res.render('users/login')
+})
+
+
 module.exports = router;
