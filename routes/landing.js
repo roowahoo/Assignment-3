232d33b330059323eb49dd884ff3a2f74b769a62
roowahoo <tohyinru95@gmail.com>
2021-04-24 16:35:58 +0000

    added landing page

--- routes/landing.js
+++ routes/landing.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
-    res.render('landing/index')
+    res.render('landing/welcome')
 })
 
 module.exports=router
