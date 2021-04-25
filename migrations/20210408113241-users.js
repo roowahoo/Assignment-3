f38f2e40a56363e85649614ef3b6bb9e6f93fa9c
roowahoo <tohyinru95@gmail.com>
2021-04-25 02:39:14 +0000

    Changed db-migrate script for users.js

--- migrations/20210408113241-users.js
+++ migrations/20210408113241-users.js
@@ -15,7 +15,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return db.createTable('users',{
+  return db.createTable('vendors',{
       id: { type: 'int', primaryKey:true, autoIncrement:true},
       username: { type: 'string', length:100},
       email: {type: 'string', length:350},
